65ab91938b74f2925137e38617d8e46ba59e46c6
evinracher <eevinley@gmail.com>
2024-06-08 08:08:04 -0500

    feat: update about description

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -6,11 +6,9 @@ export const About = () => {
     <section id="about" className={s.about}>
       <h1 className={s.title}>Welcome to my portfolio!</h1>
       <p className={s.description}>
-        As a Frontend Engineer with over 2 years of experience, I have worked
+        As a Frontend Engineer with over 4 years of experience, I have worked
         with several international companies, including a Silicon Valley-based
-        company. My stack mainly consists of React, JavaScript, TypeScript,
-        Jest, HTML, and CSS. Currently, I am working at Globant with a Disney
-        Media team on a web platform for the company.
+        company. My stack mainly consists of React, Next.js, JavaScript, TypeScript, CSS, HTML, and Jest. Currently, I am working at Wizeline with a Dow Jones team on a web platform for the company.
       </p>
     </section>
   );
